116a240b536c2da430f70fdf63b1bab67875192f
Jim Nusbaum <jim.nusbaum@vitalityindex.com>
2020-01-10 18:25:37 -0700

    initial

--- application.py
+++ application.py
@@ -81,7 +81,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.0.134", 1883, 60)
+client.connect(Config.MQTTHOST)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
